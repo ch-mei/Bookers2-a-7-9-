c9a55f9f6e0acdeb8cd9b5e4a562abc071babc03
ch-mei <mei013h0ws@gmail.com>
2021-07-28 07:52:00 +0000

    0728

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show,:index,:edit,:update]
-  resources :books
   root 'homes#top'
-  get 'homes/about' => 'homes#about'
+  get 'home/about', to: 'homes#about'
+  resources :users, only: [:show, :index, :edit, :update]
+  resources :books
+
 end
