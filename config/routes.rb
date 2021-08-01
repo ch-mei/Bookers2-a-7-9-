e58c2f97ca36b36bad5f1c79c07c4f94cd7ba27d
ch-mei <mei013h0ws@gmail.com>
2021-08-01 14:44:18 +0000

    フォロー機能作成中　0801

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about', to: 'homes#about'
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'following_user' => 'relationships#following_user', as: 'following_user'
+    get 'follower_user' => 'relationships#follower_user', as: 'follower_user'
+  end
   resources :books do
     resources :book_comments, onle: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
