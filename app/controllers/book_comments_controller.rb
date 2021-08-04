2d86a0190d65259416521c2928acc24c7fc9fe99
ch-mei <mei013h0ws@gmail.com>
2021-08-04 07:02:15 +0000

    応用課題６　いいね・コメント非同期追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,7 +2,7 @@ class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = book.id
+    @book_comment.book_id = @book.id
     if @book_comment.save
       redirect_to book_path(@book.id)
     else
