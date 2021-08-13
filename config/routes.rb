57db1aa1e223225eed44c89d65c4dfd86b8d04f6
ch-mei <mei013h0ws@gmail.com>
2021-08-13 14:32:55 +0000

    DM機能追加　非同期

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,20 @@ Rails.application.routes.draw do
 
   root 'homes#top'
   get 'home/about', to: 'homes#about'
-  
-  get 'chat/:id' => 'chats#show', as: 'chat'
+
+  get 'chat/:id' => 'chats#show', as: 'chat' #DM機能
 
   resources :users, only: [:show, :index, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
   resources :books do
     resources :book_comments, onle: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  
+
   resources :chats, only: [:create, :show]
 
 end
